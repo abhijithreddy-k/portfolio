6849631082af48a9207091dabc4517b47d9a9325
{abhijithreddy-k} <{abhijithreddy3011@gmail.com}>
2026-03-23 23:48:37 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function(){
+  emailjs.init("YOUR_PUBLIC_KEY");
+})();
+
+document.getElementById("contact-form")
+.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
+    .then(function() {
+      alert("✅ Message sent successfully!");
+    }, function(error) {
+      alert("❌ Failed to send message.");
+    });
+});
